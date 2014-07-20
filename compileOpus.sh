8b013eb828c595a3e6591d535559fc1798d74a8d
Devon Govett <devongovett@gmail.com>
2014-07-20 15:11:18 -0700

    Export emscripten module

--- compileOpus.sh
+++ compileOpus.sh
@@ -16,3 +16,4 @@ emmake make install
 cd ..
 mkdir -p build
 emcc -O3 -s EXPORTED_FUNCTIONS="['_opus_decoder_create', '_opus_decode_float', '_opus_decoder_destroy']" -Llibopus/lib -lopus -o build/libopus.js
+echo "module.exports = Module" >> build/libopus.js
