f5d1a56ffb4c07805010da1da55af8ce5bba299c
Hraban Luyat <hraban@0brg.net>
2015-02-17 15:06:54 +0000

    Better error detection during build
    emcc has unexpected exit status of 0 (success) when build fails. Not
    always, but in this particular case. So test for the existence of the
    compiled file instead as an indication of build success.

--- compileOpus.sh
+++ compileOpus.sh
@@ -16,4 +16,7 @@ emmake make install
 cd ..
 mkdir -p build
 emcc -O3 -s EXPORTED_FUNCTIONS="['_opus_decoder_create', '_opus_decode_float', '_opus_decoder_destroy']" -Llibopus/lib -lopus -o build/libopus.js
-echo "module.exports = Module" >> build/libopus.js
+# emcc does not exit with error status if the above fails, so test for output file instead
+if [ -f "build/libopus.js" ]; then
+	echo "module.exports = Module" >> build/libopus.js
+fi
